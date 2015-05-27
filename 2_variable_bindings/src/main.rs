d23a5cdefc175d329cb7d8356be90b519633a480
Lee <jessy1092@gmail.com>
2015-05-28 01:06:20 +0800

    Add variable bindings
    Signed-off-by: Lee <jessy1092@gmail.com>

---
+++ 2_variable_bindings/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let x = 5;
+
+    println!("x = {}", x);
+
+    let (x, y) = (1, 2);
+
+    println!("x = {} y = {}", x, y);
+
+    let x: i32 = 10;
+
+    println!("x = {}", x);
+
+    let mut x = 5;
+    x = 15;
+
+    println!("x = {}", x);
+}
