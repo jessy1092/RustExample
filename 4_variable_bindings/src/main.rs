5d4a93fc34e3c6753db6591eabb11dee29229840
Lee <jessy1092@gmail.com>
2022-02-03 19:09:17 +0800

    fix: Add more variable binding examples

--- 4_variable_bindings/src/main.rs
+++ 4_variable_bindings/src/main.rs
@@ -16,4 +16,38 @@ fn main() {
 
     x = 15;
     println!("x = {}", x);
+
+    let shadow_binding = 1;
+
+    {
+        println!("Inner Shadow Binding {}", shadow_binding);
+
+        let shadow_binding = 3;
+
+        println!("Inner Shadow Binding {}", shadow_binding);
+    }
+    println!("Outsider Shadow Binding {}", shadow_binding);
+
+    let shadow_binding = 2;
+    println!("Outsider Shadow Binding {}", shadow_binding);
+
+    let a_binding;
+    {
+        let x = 2;
+        a_binding = x * x;
+    }
+    println!("a binding {}", a_binding);
+
+    let mut _mutable_binding = 2;
+    {
+        let _mutable_binding = _mutable_binding;
+
+        // Error
+        // _mutable_binding = 20;
+        println!("Mutable Binding {}", _mutable_binding)
+    }
+
+    _mutable_binding = 3;
+
+    println!("Mutable Binding {}", _mutable_binding)
 }
